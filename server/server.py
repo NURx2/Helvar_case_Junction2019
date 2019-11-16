a208b9fdd4940abccf3325cbe1b0e9cd22d47330
Maksim Diakov <diakov2100@gmail.com>
2019-11-16 11:05:37 +0300

    server

---
+++ server/server.py
@@ -0,0 +1,70 @@
+from flask import Flask
+import lightControl
+from flask_socketio import SocketIO
+from flask_socketio import send, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+
+@app.route('/set/<int:level>/<int:temp>/<string:point>')
+def control_light(level, temp, point):
+	id = 1
+	if point == 'living':
+		id = 0
+	lightControl.control_light(level, temp, id)
+	return "OK"
+
+@app.route('/blink')
+def blink():
+	lightControl.blink()
+	return "OK"
+
+@app.route('/switch_on_entrance')
+def switch_on_entrance():
+	lightControl.switch_on_entrance()
+	return "OK"
+
+@app.route('/switch_off_entrance')
+def switch_off_entrance():
+	lightControl.switch_off_entrance()
+	return "OK"
+
+@app.route('/move/<string:frm>/<string:to>')
+def move(frm,to):
+
+	if frm == 'living':
+		control_light(0, 0)
+		switch_on_entrance()
+		print(1)
+
+	if frm == 'entarance':
+		if to == 'kitchen':
+			control_light(0, 0)
+		if to == 'living':
+			control_light(100, 5000)
+
+		switch_off_entrance()
+		print(2)
+
+	if frm == 'kitchen':
+		switch_on_entrance()
+		print(3)
+
+	return "OK"
+
+@socketio.on('connect')
+def test_connect():
+	print('Connected')
+
+@app.route('/send_socket/<int:id>/<int:level>/<int:temp>')
+def send_socket(id, level, temp):
+	update(id, level, temp)
+	return "OK"
+
+def update(id, level, temp):
+	socketio.emit('update', {'id': id, 'level': level, 'temp': temp })
+
+if __name__ == '__main__':
+    socketio.run(app)
